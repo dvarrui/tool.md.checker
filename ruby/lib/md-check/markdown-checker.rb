71b5b4a06b60072fff2798f361cde8ea58813eb1
David Vargas <dvarrui@protonmail.com>
2022-02-24 00:17:17 +0000

    [U] .

--- ruby/lib/md-check/markdown-checker.rb
+++ ruby/lib/md-check/markdown-checker.rb
@@ -2,6 +2,17 @@
 class MarkdownChecker
   def initialize(dirname)
     @dirname = dirname
-    p @dirname
+    @filenames = locate_md_filenames_from(@dirname)
+    check_all(@filenames)
+  end
+
+  def locate_md_filenames_from(dirname)
+    Dir.glob(File.join(dirname,'**','*.md'))
+  end
+
+  def check_all(filenames)
+    filenames.each do |filename|
+      puts "[ info ] Cheking file #{filename}"
+    end
   end
 end
