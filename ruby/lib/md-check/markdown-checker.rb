7d2f043183c23359979e970bcd951435350f12d6
David Vargas <dvarrui@protonmail.com>
2022-02-24 00:24:03 +0000

    [MYUPDATE] .

--- ruby/lib/md-check/markdown-checker.rb
+++ ruby/lib/md-check/markdown-checker.rb
@@ -2,23 +2,23 @@
 class MarkdownChecker
   def initialize(dirname)
     @dirname = dirname
-    @filenames = locate_md_filenames_from(@dirname)
-    check_all(@filenames)
+    check_dirname(@dirname)
   end
 
   def locate_md_filenames_from(dirname)
     Dir.glob(File.join(dirname,'**','*.md'))
   end
 
-  def check_all(filenames)
+  def check_dirname(dirname)
+    filenames = locate_md_filenames_from(dirname)
     puts "[ info ] Cheking files from #{@dirname}"
     filenames.each do |filename|
       puts " => Cheking file: #{filename}"
-      find_links_into filename
+      lines = get_lines_with_links_into filename
     end
   end
 
-  def find_links_into(filename)
+  def get_lines_with_links_into(filename)
     content = File.read(filename)
     lines = content.split("\n")
     puts lines.size
